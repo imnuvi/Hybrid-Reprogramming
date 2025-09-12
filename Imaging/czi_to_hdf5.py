2cba97ecdef8680cfa73d47b6cdfe81f27061c29
nuvi <ramprakashsmail@gmail.com>
2025-09-12 14:49:43 -0400

    ADD: configurable mosaic overlap code

--- Imaging/czi_to_hdf5.py
+++ Imaging/czi_to_hdf5.py
@@ -16,10 +16,6 @@ import sys
 
 
 
-args = sys.argv
-scene_name, scene, counter = args[1], int(args[2]), int(args[3])
-
-
 
 czi_0_24_hours = '2024-12-21-PRRX1-MYOD-3-01.czi'
 base_pth = Path("/scratch/indikar_root/indikar1/shared_data/imaging_test/full_CZIs")
@@ -51,7 +47,7 @@ def save_scene_to_hdf5(scene_array, scene, count, timepoint):
         f.create_dataset("channel_names", data=np.array(["tagGFP", "MKate", "Cy5"], dtype=dt))
 
 
-def save_frame_to_hdf5(frame, scene, count, timepoint):
+def save_frame_to_hdf5(frame, scene, count, timepoint, basepath):
     """
     Save a single frame into a HDF5 file
     """
@@ -59,8 +55,8 @@ def save_frame_to_hdf5(frame, scene, count, timepoint):
     
     dt = h5py.string_dtype(encoding='utf-8')  
     # Saving to HDF5
-    bpath = f"/nfs/turbo/umms-indikar/Ram/projects/reprogramming/Image_analysis/convertor/{scene}/{count}"
-    bpath = f"/scratch/indikar_root/indikar1/shared_data/ImageProcessing/{scene}/{count}"
+    # bpath = f"/nfs/turbo/umms-indikar/Ram/projects/reprogramming/Image_analysis/convertor/{scene}/{count}"
+    bpath = f"{basepath}/{scene}/{count}"
     os.makedirs(bpath, exist_ok=True)
     with h5py.File(f"{bpath}/t_{timepoint}.hdf5", "w") as f:
         dset = f.create_dataset("Scene", data=data, compression="gzip")
@@ -74,18 +70,41 @@ def save_frame_to_hdf5(frame, scene, count, timepoint):
 
 
 def proc_czi(czi, scene, scene_name, counter, timedelay):
+
     metadata = czi.meta
     my_dims = czi.dims
     my_size = czi.size
     dimensions = czi.get_dims_shape()
     print(my_dims)
+    print(dimensions)
     
     x = dimensions[0]['X'][1]
     y = dimensions[0]['Y'][1]
-    timepoints = dimensions[0]['T'][1] - 1
-    channels = 4
-    rows = 6
-    columns = 5
+    
+    timepoints = dimensions[0]['T'][1] 
+    channels = dimensions[0]['C'][1]
+    mosaic = dimensions[0]['M'][1]
+    # rows = 6
+    # columns = 5
+    
+    # channels = 3
+    rows = 4
+    columns = 4
+
+    # for some stitches the row order gets flipped in the software. so the ordering should be reversed in alternating rows
+    invert = True
+    xoverlap = 22 # The number of pixels that overlap on both sides
+    yoverlap = 27
+    
+    print(f'Timepoints: {timepoints}')
+    print(f'Channels: {channels}')
+    print(f'Rows: {rows}')
+    print(f'Columns: {columns}')
+    print(f'X: {x}')
+    print(f'Y: {y}')
+    print(f'Mosaic: {mosaic}')
+    
+
     
     timeframes = []
     # Iterate timepoints collecting each frame
@@ -95,12 +114,22 @@ def proc_czi(czi, scene, scene_name, counter, timedelay):
         for c in range(channels):
             full_image = []
             # Iterating through each mosaic segment
-            for i in range(rows * columns):
-                img, shp = czi.read_image(C=c, S=scene, Z=0, T=t, M=i)
-                proc_img = img[0, 0, 0, 0, 0, 0, ::, ::]
-            
-                full_image.append(proc_img)
-            
+        
+            for i in range(rows):
+                for j in range(columns):
+                    
+                    if i%2 == 1 and invert:
+                        # jind = ((i*j) - j)
+                        pos = (columns * i) + (columns - j) - 1
+                    else:
+                        pos = (columns * i) + j
+
+                    img, shp = czi.read_image(C=c, S=scene, Z=0, T=t, M=pos)
+                    proc_img = img[0, 0, 0, 0, 0, 0, ::, ::]
+                    proc_img = proc_img[xoverlap:-xoverlap, yoverlap:-yoverlap]
+                
+                    full_image.append(proc_img)
+                        
             grid = np.empty(rows * columns, dtype=object)
             grid[:] = full_image
             
@@ -121,15 +150,20 @@ def proc_czi(czi, scene, scene_name, counter, timedelay):
             # ])
             multi_channel.append(stitched)
         frame = np.stack(multi_channel, axis=0)
-        save_frame_to_hdf5(frame, scene_name, counter, timedelay + t)
+        # save_frame_to_hdf5(frame, scene_name, counter, timedelay + t)
+        return frame, metadata
 
 
 
 scenemap = {"Negative_Controls": [1, 7, 13], "Myod": [2, 3], "PRRX1": [14, 15], "Myod_PRRX1": [8, 9, 11]}
 
+if __name__ == '__main__':
+    
+    args = sys.argv
+    scene_name, scene, counter = args[1], int(args[2]), int(args[3])
 
 
-print("Running Scene", scene_name, counter)
-proc_czi(czi_0_24, scene, scene_name, counter, timedelay = 0)
-proc_czi(czi_24_72, scene, scene_name, counter, timedelay = 67)
-print("saving Scene", scene_name, counter)
+    print("Running Scene", scene_name, counter)
+    proc_czi(czi_0_24, scene, scene_name, counter, timedelay = 0)
+    proc_czi(czi_24_72, scene, scene_name, counter, timedelay = 67)
+    print("saving Scene", scene_name, counter)
